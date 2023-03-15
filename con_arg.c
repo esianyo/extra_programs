fdc972d5fbd8c9f2c37a5d84d57fb9f9b6292e74
Esianyo Dzisenu <esianicd@gmail.com>
2023-03-15 01:39:40 +0000

    a function that concatenates all the arguments of your program.

---
+++ con_arg.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+char *argstostr(int ac, char **av)
+{
+    int i, j, len = 0, k = 0;
+    char *str;
+
+    if (ac == 0 || av == NULL)
+        return (NULL);
+
+    for (i = 0; i < ac; i++)
+    {
+        for (j = 0; av[i][j]; j++)
+            len++;
+        len++;
+    }
+
+    str = malloc(sizeof(char) * (len + 1));
+
+    if (str == NULL)
+        return (NULL);
+
+    for (i = 0; i < ac; i++)
+    {
+        for (j = 0; av[i][j]; j++)
+            str[k++] = av[i][j];
+        str[k++] = '\n';
+    }
+
+    str[k] = '\0';
+
+    return (str);
+}
+

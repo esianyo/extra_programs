3954391793c8414a90f3679b10f7440a8ce8185b
root <root@esianyo.localdomain>
2023-04-25 21:22:55 +0000

    update

---
+++ nums.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers below 10
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while(i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	return (0);
+}

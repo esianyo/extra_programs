9839f11293a0ac6f621288d1a2b184d792d32ed1
Esianyo Dzisenu <esianicd@gmail.com>
2023-05-19 00:25:22 +0000

    printing numbers from 1 to 49 using while loop

---
+++ c_programs/nums_2to49a.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers between 1 and 50
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	i = i + 1;
+	while (i <= 49)
+	{
+		printf("%d ", i);
+		i = i + 1;
+	}
+	printf("\n");
+	return (0);
+}
